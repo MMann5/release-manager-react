499456bf8318fae30c3952580498bab0e95f293c
Michael Mann <mmann_ext@christiandior.com>
2024-04-10 19:04:24 +0300

    Refactor rendering logic in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -76,34 +76,35 @@ const Home = () => {
       <SkeletonCmp display={loading} />
       <ErrorCmp display={!loading && error} retry={getConfigTable} />
       <NoDataCmp display={!loading && Object.values(allData)?.length === 0} />
-      !loading && {Object.keys(allData).map((key: string, index: number) => {
-        return (
-          <div
-            key={index}
-            style={{
-              display: "flex",
-              margin: "10px",
-              padding: "10px",
-              border: "1px solid black",
-              borderRadius: "5px",
-              gap: "10px",
-              justifyContent: "space-between",
-            }}
-          >
-            <div>{key}</div>
-            {allData[key].map((repo: Data, index: number) => {
-              return (
-                <div
-                  key={index}
-                  style={{ display: "flex", flexDirection: "column" }}
-                >
-                  <div>{repo.version}</div>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+      {!loading &&
+        Object.keys(allData).map((key: string, index: number) => {
+          return (
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                margin: "10px",
+                padding: "10px",
+                border: "1px solid black",
+                borderRadius: "5px",
+                gap: "10px",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>{key}</div>
+              {allData[key].map((repo: Data, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    style={{ display: "flex", flexDirection: "column" }}
+                  >
+                    <div>{repo.version}</div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 };
